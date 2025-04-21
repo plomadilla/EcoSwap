219c5f2d281f2b1738f9742fef551f9b70d18f95
plomadilla <patiytimonhasperue@gmail.com>
2025-04-20 22:07:17 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 Importamos BrowserRouter
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/EcoSwap"> {/* 👈 basename agregado */}
+    <App />
+  </BrowserRouter>
+);
